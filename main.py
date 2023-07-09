4a46b0dcb376705c4ac9f039fa2f5d3d8c98aa25
A1an <387805107@qq.com>
2023-07-10 00:22:04 +0800

    移除了websocket接口的功能

--- main.py
+++ main.py
@@ -7,27 +7,6 @@ from server import web
 if __name__ == '__main__':
     # 初始化行情模块
     init_binance()
-
-    # def message_handler(_, message):
-    #     result = json.loads(message)
-    #     print(result)
-    #     print(type(result))
-    #
-    # websocketClient = UMFuturesWebsocketClient(stream_url=config['binance']['stream_url'],on_message=message_handler)
-    #
-    # websocketClient.kline(
-    #     symbol='BTCUSDT',
-    #     interval="1m",
-    # )
-
-    # websocketClient.kline(
-    #     symbol='LINKUSDT',
-    #     interval="1m",
-    # )
-
-    # time.sleep(60*60*24)
-    # logger.debug("closing ws connection")
-    # websocketClient.stop()
     # 启动行情推送
 #     feishu.send(f'''
 # {config["app"]["name"]}@{config["app"]["version"]}
