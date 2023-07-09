e62c09560a3e4f1ecefdefe04d0fb6d9f78ade1e
A1an <387805107@qq.com>
2023-07-10 00:44:23 +0800

    添加了requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import feishu
 from binance_util import init_binance
-from core import config
-from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
-from server import web
 
 if __name__ == '__main__':
     # 初始化行情模块
